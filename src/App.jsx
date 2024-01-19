1b43b4791468c7c4d47c6765bda65a20c5e44c55
Karthikraut <karthikraut2@gmail.com>
2024-01-19 23:21:33 +0530

    Newsletter code Has been Written

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Newsletter from './components/newsletter.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1 className='font-bold underline'>ACES Website</h1>
-       
+      <Newsletter/> 
     </>
   )
 }
 
-export default App
+export default App;
