553b742d8591253fc82317f3ff00887b1322e381
kartik <kartikb.5578@gmail.com>
2024-01-31 23:27:57 +0530

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Newsletter from './components/newsletter.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <Newsletter/> 
+      <Footer/>
     </>
   )
 }
