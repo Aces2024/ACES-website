662e64b612bc654fb8b91b0f28dc27ec174e9411
kartik <kartikb.5578@gmail.com>
2024-02-05 12:41:22 +0530

    social added in footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Newsletter from './components/newsletter.jsx'
 import Footer from './components/Footer.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Newsletter/> 
+      <BrowserRouter>
+      <Routes>
+      <Route path="/" element={<Home/>} />
+        <Route path="/newsletter" element={<Newsletter/>} />
+      </Routes>
       <Footer/>
+      </BrowserRouter>
     </>
   )
 }
